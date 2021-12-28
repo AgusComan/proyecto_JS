5fdd503027cfa26e359272e3d0fd914d7b13059d
sergio agustin coman <agusser17@gmail.com>
2021-12-28 01:16:01 -0300

    Desafio Variables Terminado

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 alert(`Hola! A continuacion le vamos a pedir que ingrese dos numeros para realizar operaciones matematicas.`);
 
+//Mediante un promt y su correspondiente parseo se le asigna una variable para que el contenido quede guardado en ella
 let primerNumero = parseFloat(prompt(`Ingrese un numero: `));
 let segundoNumero = parseFloat(prompt(`Ingrese otro numero: `));
 
+//Luego se crea la alert y la muestras por consola para mostrar los resultados
+//Dentro de estas se usa los "\t" por estetica
+// Y tambien se uso los Back ticks para poner dentro las operaciones
 alert(`Los resultados son: \t
        Suma: ${primerNumero + segundoNumero} \t
        Resta: ${primerNumero - segundoNumero} \t

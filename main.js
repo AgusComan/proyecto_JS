37ff818f78e6c049b13e0c2dca86a4b245a1b57d
sergio agustin coman <agusser17@gmail.com>
2022-01-11 17:23:59 -0300

    Desafio ciclos

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
-/*Solicitar al usuario dos numeros, y una operacion. Utilizando un condicional realizar la operacion solicitada 
-por el usuario, y mostrar mediante un alert el resultado. (Verificar que los datos ingresados sean correctos).*/
+/* Ingresar 10 numeros y mostrar cuales son pares e impares */
 
-//solicito el ingreso de los datos.
-let operacion = prompt(`Hola! Ingrese una operacion matematica para resolver`).toLowerCase();
-let primerNumero = parseFloat(prompt(`Ingrese un numero`));
-let segundoNumero = parseFloat(prompt(`Ingrese otro numero`));
-
-//Se realizan if y else if para identificar la operacion que ingreso el usuario y dentro de estas calcular la operacion elegida.
-if(operacion == "suma"){
-       console.log(`La ${operacion} es: ${primerNumero + segundoNumero}`);
-}
-else if(operacion == "resta"){
-       console.log(`La ${operacion} es: ${primerNumero - segundoNumero}`);
-}
-else if(operacion == "multiplicacion"){
-       console.log(`La ${operacion} es: ${primerNumero * segundoNumero}`);
-}
-else if(operacion == "division"){
-       console.log(`La ${operacion} es: ${primerNumero / segundoNumero}`);
-}
-else if(operacion == "potencia"){
-       console.log(`La ${operacion} es: ${primerNumero ** segundoNumero}`);
-}
-else if(operacion == "resto"){
-       console.log(`La ${operacion} es: ${primerNumero % segundoNumero}`);
-}
-else{
-       console.log("La operacion no es posible");
+for(let i = 0 ; i < 10 ; i++){
+       let numeroIngrasado = Number(prompt("ingrese un numero"));
+       if(numeroIngrasado%2 == 0){
+              console.log(`El numero ingresado ${numeroIngrasado} es par`);
+       }
+       else{
+              console.log(`El numero ingresado ${numeroIngrasado} es impar`);
+       }
 }

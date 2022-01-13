e18a8f03a5e9d69f7a7a8aa34607fa224a7a7096
sergio agustin coman <agusser17@gmail.com>
2022-01-13 20:27:29 -0300

    desafio funciones

--- main.js
+++ main.js
@@ -1,32 +1,16 @@
-//1. Generar una funcion suma, donde el usuario ingresa dos numeros y la funcion devuelve el resultado de la suma de dichos numeros.
+//Calcular edad promedio de personas registradas.
 
-/*const Suma = function(primerNumero, segundoNumero){
-       return `La suma es: ${primerNumero + segundoNumero}`;
-}
+let cantidadPersonas = Number(prompt("Hola! ingresa la cantidad de personas para ingresar su edad"));
+let promedio;
+let acumulador = 0;
 
-let primerNumero = Number(prompt("Hola! ingrese un numero"));
-let segundoNumero = Number(prompt("Ingrese otro numero"));
-
-console.log(Suma(primerNumero, segundoNumero));*/
-
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-//2. Realice un programa que simule, mediante una función como el ordenador cuenta por consola, 
-//   desde 0 hasta un número ingresado por el usuario.
-
-/*const Simulador = numeroIngresado => {
-       for(let i = 0; i <= numeroIngresado; i++){
-              console.log(i);
+const Edades = (cantidad) => {
+       for(let i=1; i<=cantidad; i++){
+              let ingresoEdades = Number(prompt("ingrese las edades:"));
+              acumulador = acumulador + ingresoEdades;
+              promedio = acumulador/i;
        }
+       return `La edad promedio de las ${cantidadPersonas} personas es de: ${promedio}`;
 }
 
-let ingreso = Number(prompt("Hola! ingrese un numero para contar"));
-
-Simulador(ingreso);*/
-
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// Codificar dos funciones:
-// *Una función cotizarDolar(pesos), la cual recibe un valor monetario en pesos argentinos, y lo retorna en su equivalente en dólares.
-// *Una función cotizarPesos(dolar), que recibe un valor monetario en dólares, y lo retorna en su equivalente en pesos argentinos.
-// Luego invocar las funciones solicitando  al usuario el valor y el tipo de cotización a realizar.
+alert(Edades(cantidadPersonas));

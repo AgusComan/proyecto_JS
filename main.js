a1873f31015d53968354cb751e73434c37ed723b
sergio agustin coman <agusser17@gmail.com>
2022-01-13 04:21:01 -0300

    practicando funciones

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-/* Ingresar n numeros y mostrar cuales son pares e impares */
+//1. Generar una funcion suma, donde el usuario ingresa dos numeros y la funcion devuelve el resultado de la suma de dichos numeros.
 
-let cantidad = parseInt(prompt("Hola! Ingrese la cantidad de numeros a ingresar:"));
+/*const Suma = function(primerNumero, segundoNumero){
+       return `La suma es: ${primerNumero + segundoNumero}`;
+}
 
-for(let i = 0 ; i < cantidad ; i++){
-       let numeroIngrasado = Number(prompt("ingrese un numero"));
-       if(numeroIngrasado%2 == 0){
-              console.log(`El numero ingresado ${numeroIngrasado} es par`);
-       }
-       else{
-              console.log(`El numero ingresado ${numeroIngrasado} es impar`);
+let primerNumero = Number(prompt("Hola! ingrese un numero"));
+let segundoNumero = Number(prompt("Ingrese otro numero"));
+
+console.log(Suma(primerNumero, segundoNumero));*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//2. Realice un programa que simule, mediante una función como el ordenador cuenta por consola, 
+//   desde 0 hasta un número ingresado por el usuario.
+
+/*const Simulador = numeroIngresado => {
+       for(let i = 0; i <= numeroIngresado; i++){
+              console.log(i);
        }
 }
+
+let ingreso = Number(prompt("Hola! ingrese un numero para contar"));
+
+Simulador(ingreso);*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Codificar dos funciones:
+// *Una función cotizarDolar(pesos), la cual recibe un valor monetario en pesos argentinos, y lo retorna en su equivalente en dólares.
+// *Una función cotizarPesos(dolar), que recibe un valor monetario en dólares, y lo retorna en su equivalente en pesos argentinos.
+// Luego invocar las funciones solicitando  al usuario el valor y el tipo de cotización a realizar.

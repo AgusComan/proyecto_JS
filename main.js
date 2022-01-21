65e0f3895ab3dd9162d4d29134811aec3ead4971
sergio agustin coman <agusser17@gmail.com>
2022-01-20 22:45:46 -0300

    Desafio arrays y 1° Entrega Terminado

--- main.js
+++ main.js
@@ -1,30 +1,75 @@
+/*QObjetivo: que el usuario pueda escribir diferentes nombres de usuarios con sus respectiva duracion del juego (simulado) guardado en un array
+de objetos y que de esta manera se puedan ordenar un ranking.*/
+
+
 //declaracion de variables a uutilizar
 let iniciarSesion;
+let tiempoDeJuego;
+let contador = 0;
+let cont = 0;
+let arrayUsuarios = [];
 
-//funcion para entrada de datos
+
+//funcion para registro de usuarios
 const ingreso = () => {
        iniciarSesion = prompt("Hola! ingrese sesion con su nombre de usuario.");
-       return `${iniciarSesion}`
+       tiempoDeJuego = Number(prompt("Ingrese el tiempo de duracion del juego. Solo ingrese numeros enteros sin su unidad de medida, el valor se lo tomara en MINUTOS"));
+       contador ++;
 }
 
-//creacion de objetos usando el metodo de clases
+
+//creacion de objetos usando el metodo de clases para guardar los datos de los usarios registrados
 class Usuarios {
-       constructor(nombre){
+       constructor(id , nombre , DuracionDelJuego){
+              this.id = id;
               this.nombre = nombre;
+              this.DuracionDelJuego = DuracionDelJuego;
        }
 
+       //con este metodo se verifica si el usuario ingreso o no el nombre de usuario. Depende la condicion se ejecuta un aviso
        verificacion(ingresoUsuario){
               if(ingresoUsuario != ""){
-                     alert(`Bienvenido ${this.nombre}! Ingreso exitoso. ¿Listo para jugar?`);
+                     console.log(`Bienvenido ${this.nombre}! Ingreso exitoso. ¿Listo para jugar?`);
               }
               else{
-                     alert(`Error!! Hubo un problema al iniciar sesion.`);
-                     console.error(`Error!! Hubo un problema al iniciar sesion.`);
+                     console.warn(`Error!! Hubo un problema al iniciar sesion.`);
               }
        }
 }
 
 
-ingreso();
-const usuario1 = new Usuarios (iniciarSesion);
-usuario1.verificacion();
+//inicio de la "estructura principal"
+//comienza con una aclaracion y una entrada de datos para evaluar condiciones y ciclos
+inicio = prompt("Bienvenido! te voy a pedir que ingreses, para simular, los nombres de usuarios y su tiempo de juego de las personas que quieras para determinar quien sale primero tomando en cuenta la menor duracion. \n Para finalizar los ingresos ingrese 'FIN' \n De lo contrario apretar 'ENTER'").toLowerCase();
+
+//El ciclo de reproduce hasta que el usuario lo finalize
+while(inicio != "fin"){
+
+       //llamada  a la funcion para registro de datos
+       ingreso();
+
+       //Se crea una instancia para los objetos con sus parametros
+       const usuarioIngresado = new Usuarios (contador , iniciarSesion , tiempoDeJuego);
+       usuarioIngresado.verificacion(iniciarSesion);
+
+       //se cargan los usuarios que se ingresen a un array vacio
+       arrayUsuarios.push(usuarioIngresado);
+
+       //vuelve a pedir una entrada de datos para continuar o no el ciclo
+       inicio = prompt("Bienvenido! te voy a pedir que ingreses, para simular, los nombres de usuarios y su tiempo de juego de las personas que quieras para determinar quien sale primero tomando en cuenta la menor duracion. \n Para finalizar los ingresos ingrese 'FIN' \n De lo contrario apretar 'ENTER'").toLowerCase();
+}
+
+
+//con el metodo .sort() se compara entre la duracion de juego de cada usuario asi ordenandolo de menor a mayor por medio de una resta y devolviendo un numero.
+//este numero puede ser positivo, negativo o cero.
+arrayUsuarios.sort(function(a, b){return a.DuracionDelJuego - b.DuracionDelJuego});
+
+console.log(`El ranking quedo de la siguiente manera:`);
+
+//se utiliza el for of para que recorra el array ya ordenado y muestre los resultados
+for(const ranking of arrayUsuarios){
+
+       //con un contador demuestran en que posición quedo cada jugador
+       cont ++;
+       console.log(`En ${cont}° Lugar ${ranking.nombre} con un tiempo de ${ranking.DuracionDelJuego} minutos`);
+}
